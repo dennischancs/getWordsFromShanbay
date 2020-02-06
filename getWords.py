082323e9a0aa20bbe430fcf72e4a551897c09777
dennischan <48424802+dennischancs@users.noreply.github.com>
2020-02-06 13:15:49 +0800

    Update getWords.py

--- getWords.py
+++ getWords.py
@@ -20,11 +20,8 @@ def loadJSON(filename):
         data = json.load(fileObject)
         return data
 
-
-# 可以手动预设 categories
-# 比如已爬取到 wordlist，之后爬取 words，不用从头再来，可以直接写成这样：
-# categories = [] # 从重新爬取
-categories = loadJSON('wordlist') # 但2020/02/06测试时，单词书连接已经失效，只能利用原wordlist
+# 2020/02/06测试时，单词书连接已经失效，只能利用已爬取到的 wordlist 预设 categories
+categories = loadJSON('wordlist')
 
 # 设置需要爬取的单词书类别 category 和数量 count
 selectBooks = [
@@ -53,9 +50,6 @@ selectBooks = [
     {'category': '其他', 'count': '137'}
 ]
 
-# 模拟登录
-# [Python3.6下的Requests登录及利用Cookies登录 - 西柳居士 - 博客园](https://www.cnblogs.com/ddddfpxx/p/8624715.html)
-
 # 预设 url
 urls = {
     'base': 'https://www.shanbay.com/',
@@ -79,13 +73,15 @@ headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,fr-FR;q=0.6,fr;q=0.5,ja;q=0.4,de-DE;q=0.3,de;q=0.2,zh-TW;q=0.1,und;q=0.1'
 }
-# 填写登陆后的浏览器cookies
+
+# 填写登陆后的浏览器Cookies
+# [Python3.6下的Requests登录及利用Cookies登录 - 西柳居士 - 博客园](https://www.cnblogs.com/ddddfpxx/p/8624715.html)
 cookies = {
-    'csrftoken': 'DA9Is2Et4jnSMo****URI5lyvvBnxOD',
-    '_ga': 'GA1.2.1767870350.1580901205',
-    'sensorsdata2015jssdkcross': '%7B%22distinct_id%22%3A%22tlsyvn%22%2C%22first_id%22%3A%2217015103c6d1e6-025c894ac78401-b383f66-1327104-17015103c6e648%22%2C%22props%22%3A%7B%22%24latest_referrer%22%3A%22%22%2C%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22%24device_id%22%3A%2217015103c6d1e6-025c894ac78401-b383f66-1327104-17015103c6e648%22%7D',
-    'sessionid': '"e30:1izPFD:FtbuI-pzlSu99oTUUgucx7kQhoo"',
-    'auth_token': 'eyJ0eXAiOiJKV1****ciOiJIUzI1NiJ9.eyJpZCI6NTY2Njc3ODcsImV4cCI6MTU4MTc5MDk3OSwiZGV2aWNlIjoiIiwidXNlcm5hbWUiOiJEZW5uaXNfQ2hhbiIsImlzX3N0YWZmIjowfQ.4XMRFlsFJ9rL4CaF6_jHDBpKPCN3FOZAu4hTGbNSPx4'
+    'csrftoken': 'AAAAAAAAAAAAAAAAAAAAAA',
+    '_ga': 'AAAAAAAAAAAAAAAAAAAAAA5',
+    'sensorsdata2015jssdkcross': 'AAAAAAAAAAAAAAAAAAAAAA',
+    'sessionid': '"AAAAAAAAAAAAAAAAAAAAAA"',
+    'auth_token': 'AAAAAAAAAAAAAAAAAAAAAA4'
 }
 
 # 由于本人不会模拟滑动验证码，暂时采用Cookies登陆(Cookies有效期10天)
@@ -96,52 +92,6 @@ def login():
 # 创建 session
 s = requests.session()
 print('建立连接 ')
-
-# # 爬取单词书类目
-# def getCategories():
-#     r = s.get(urls['categories'], headers=headers['base'])
-#     print('单词书类别 ', r.status_code)
-#     print('开始解析')
-#     soup = bs4.BeautifulSoup(r.text, features='lxml')
-#     tags_li = soup.find(id='wordbook-category-list').find_all('li')
-#     for tag_li in tags_li:
-#         data = tag_li['data']
-#         category = tag_li.text
-#         categories.append({'id': data, 'category': category})
-#     print('完成解析')
-#     saveJSON(categories, 'categories')
-
-# # 爬取单词书列表
-# def getWordbooks():
-#     for category in categories:
-#         r = s.get(urls['category'] + category['id'])
-#         soup = bs4.BeautifulSoup(r.text, features='lxml')
-#         tags_book = soup.find_all(class_='wordbook-title')
-#         category['wordbooks'] = []
-#         for tag_book in tags_book:
-#             title = tag_book.text
-#             href = tag_book.find('a')['href']
-#             category['wordbooks'].append({'title': title, 'href': href})
-#     saveJSON(categories, 'wordbooks')
-
-# 爬取单词列表
-# def getWordlist():
-#     for category in categories:
-#         for wordbook in category['wordbooks']:
-#             r = s.get(urls['base'] + wordbook['href'])
-#             soup = bs4.BeautifulSoup(r.text, features='lxml')
-#             tags_worditem = soup.find_all(class_='wordbook-wordlist-name')
-#             tags_wordcount = soup.find_all(class_='wordbook-wordlist-count')
-#             print(tags_worditem)
-#             wordbook['wordlist'] = []
-#             for i in range(len(tags_worditem)):
-#                 title = tags_worditem[i].text
-#                 href = tags_worditem[i].find('a')['href']
-#                 count = tags_wordcount[i].text
-#                 print(title, href, count)
-#                 wordbook['wordlist'].append(
-#                     {'title': title, 'href': href, 'count': count})
-#     saveJSON(categories, 'wordlist')
 
 # 爬取单词
 def getWords(selectBooks):
@@ -182,7 +132,4 @@ def getWords(selectBooks):
 # 开始执行
 if __name__ == '__main__':
     login()
-    # getCategories()
-    # getWordbooks()
-    # getWordlist()
     getWords(selectBooks)
